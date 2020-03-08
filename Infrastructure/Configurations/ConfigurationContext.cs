708ffcdbeeb3ac010e8b718e22a1b266d079ff7c
Vinicius <vini.pierina@gmail.com>
2020-03-08 18:44:22 -0300

    Configurating the Infrastructure

---
+++ Infrastructure/Configurations/ConfigurationContext.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Configurations
+{
+    public sealed class ConfigurationContext : DbContext
+    {
+        public ConfigurationContext(DbContextOptions<ConfigurationContext> options) : base(options)
+        {
+            Database.Migrate();
+        }
+        public DbSet<Guia> Guias { get; set; }
+        public DbSet<Agendamento> Agendamentos { get; set; }
+    }
+}
