a154cb899a88479c6bfb815d9253cff3a02653f0
Vinicius <vini.pierina@gmail.com>
2020-04-12 10:38:22 -0300

    Adding Contrato in the Domain

---
+++ Domain/Commands/CreateContratoCommand.cs
@@ -0,0 +1,19 @@
+﻿using Core.CQRS;
+
+namespace Domain.Commands
+{
+    public class CreateContratoCommand : Command
+    {
+        public CreateContratoCommand(string codTit, string nomeTit, string situCont, string cpfTit)
+        {
+            Codtit = codTit;
+            Nometit = nomeTit;
+            Situcont = situCont;
+            Cpftit = cpfTit;
+        }
+        public string Codtit { get; set; }
+        public string Nometit { get; set; }
+        public string Situcont { get; set; }
+        public string Cpftit { get; set; }
+    }
+}
---
+++ Domain/Commands/UpdateContratoCommand.cs
@@ -0,0 +1,24 @@
+﻿using Core.CQRS;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Commands
+{
+    public class UpdateContratoCommand : Command
+    {
+        public UpdateContratoCommand(Guid id, string codTit, string nomeTit, string situCont, string cpfTit)
+        {
+            Id = id;
+            Codtit = codTit;
+            Nometit = nomeTit;
+            Situcont = situCont;
+            Cpftit = cpfTit;
+        }
+        public Guid Id { get; set; }
+        public string Codtit { get; set; }
+        public string Nometit { get; set; }
+        public string Situcont { get; set; }
+        public string Cpftit { get; set; }
+    }
+}
