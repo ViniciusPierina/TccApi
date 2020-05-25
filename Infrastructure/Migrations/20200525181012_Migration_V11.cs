c811efb5d9384d77f8128b089f02f334968e2fd4
Vinicius <vini.pierina@gmail.com>
2020-05-25 15:42:40 -0300

    Adding a new migration

---
+++ Infrastructure/Migrations/20200525181012_Migration_V11.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class Migration_V11 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Guias_ContratoId",
+                table: "Guias");
+
+            migrationBuilder.DropColumn(
+                name: "GuiaId",
+                table: "Contratos");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Guias_ContratoId",
+                table: "Guias",
+                column: "ContratoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Guias_ContratoId",
+                table: "Guias");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "GuiaId",
+                table: "Contratos",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Guias_ContratoId",
+                table: "Guias",
+                column: "ContratoId",
+                unique: true);
+        }
+    }
+}
