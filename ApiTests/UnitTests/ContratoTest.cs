2b3dd8195991aba5b55e0d0aa79209118c29eead
Vinicius <vini.pierina@gmail.com>
2020-09-11 17:14:17 -0300

    Adding the unit tests

---
+++ ApiTests/UnitTests/ContratoTest.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Core.CQRS;
+using Domain.Interfaces;
+using Domain.Models;
+using Moq;
+using NUnit.Framework;
+using Services.Mappings;
+using Services.Services;
+using System;
+
+namespace ApiTests.UnitTests
+{
+	public class ContratoTest
+	{
+		public ContratoTest()
+		{
+		}
+
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void When_RepositoryReturnsNull_Then_ResultNull()
+		{
+			var repoMock = new Mock<IRepository<Contrato>>();
+			Contrato expected = null;
+			repoMock.Setup(x => x.Find(It.IsAny<Guid>())).Returns(expected);
+
+			var mapper = new MapperConfiguration(config => config.AddProfile<MappingProfiles>()).CreateMapper();
+			var busMock = new Mock<IBus>();
+			var service = new ContratoService(repoMock.Object, mapper, busMock.Object);
+
+			var result = service.Get(Guid.NewGuid());
+
+			Assert.IsNull(result);
+		}
+	}
+}
---
+++ ApiTests/UnitTests/GuiaTest.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using Core.CQRS;
+using Domain.Interfaces;
+using Domain.Models;
+using Moq;
+using NUnit.Framework;
+using Services.DTOs;
+using Services.Mappings;
+using Services.Services;
+using Services.Services.Interfaces;
+using System;
+
+namespace ApiTests.UnitTests
+{
+	public class GuiaTest
+	{
+		public GuiaTest()
+		{
+		}
+
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void When_RepositoryReturnsNull_Then_ResultNull()
+		{
+			var repoMock = new Mock<IRepository<Guia>>();
+			Guia expected = null;
+			repoMock.Setup(x => x.Find(It.IsAny<Guid>())).Returns(expected);
+
+			var serviceMock = new Mock<IContratoService>();
+
+			var mapper = new MapperConfiguration(config => config.AddProfile<MappingProfiles>()).CreateMapper();
+			var busMock = new Mock<IBus>();
+			var service = new GuiaService(repoMock.Object, mapper, busMock.Object, serviceMock.Object);
+
+			var result = service.Get(Guid.NewGuid());
+
+			Assert.IsNull(result);
+		}
+
+		[Test]
+		public void When_ContratoIsInativo_Then_ThrowsArgumentException()
+		{
+			var repoMock = new Mock<IRepository<Guia>>();
+			Guia guiaExpected = null;
+			repoMock.Setup(x => x.Find(It.IsAny<Guid>())).Returns(guiaExpected);
+
+			var contratoServiceMock = new Mock<IContratoService>();
+			ContratoDTO contratoExpected = new ContratoDTO();
+			contratoExpected.Situcont = "Inativo";
+			contratoServiceMock.Setup(x => x.Get(It.IsAny<Guid>())).Returns(contratoExpected);
+
+			var mapper = new MapperConfiguration(config => config.AddProfile<MappingProfiles>()).CreateMapper();
+			var busMock = new Mock<IBus>();
+			var service = new GuiaService(repoMock.Object, mapper, busMock.Object, contratoServiceMock.Object);
+
+			CreateGuiaDTO guiaDTO = new CreateGuiaDTO();
+
+			Assert.Throws(typeof(ArgumentException),
+				new TestDelegate(() => service.Save(guiaDTO)));
+		}
+	}
+}
